afd07a3a2615f25e8cedc590b4d26150872d60ef
Tanishq Joshi <58567550+tanishqj2005@users.noreply.github.com>
2020-09-25 19:18:39 +0530

    Initialized App

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ textAlign: "center", color: "#320faf" }}>
+        This is the foundation of Muzik App! I will start working on it from
+        next week. Very Excited Though!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +17,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffcbab",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 30,
   },
 });
